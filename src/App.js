a8428d0146d57ac925022d2bea6a996248f719db
muchirijane <muchirijane@outlook.com>
2021-06-21 09:51:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>React cryptocurrency tracker </h1>
+			<h2>Cryptocurrency Prices by Market Cap</h2>
+		</div>
+	);
+}
+
+export default App;
